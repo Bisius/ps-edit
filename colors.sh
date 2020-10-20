1eb61afea71093a4b47f810fe69aec449ba9ca0c
Lorenzo Bisello <bisius98@gmail.com>
2020-10-20 10:25:49 +0200

    Added colors definitions

---
+++ colors.sh
@@ -0,0 +1,27 @@
+# colors.sh
+
+NC="\[\e[0m\]"
+
+BLACK="\[\e[30m\]"
+RED="\[\e[31m\]"
+GREEN="\[\e[32m\]"
+YELLOW="\[\e[33m\]"
+BLUE="\[\e[34m\]"
+PURPLE="\[\e[35m\]"
+CYAN="\[\e[36m\]"
+
+BLACKB="\[\e[1;30m\]"
+REDB="\[\e[1;31m\]"
+GREENB="\[\e[1;32m\]"
+YELLOWB="\[\e[1;33m\]"
+BLUEB="\[\e[1;34m\]"
+PURPLEB="\[\e[1;35m\]"
+CYANB="\[\e[1;36m\]"
+
+BLACKU="\[\e[4;30m\]"
+REDU="\[\e[4;31m\]"
+GREENU="\[\e[4;32m\]"
+YELLOWU="\[\e[4;33m\]"
+BLUEU="\[\e[4;34m\]"
+PURPLEU="\[\e[4;35m\]"
+CYANU="\[\e[4;36m\]"
--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 
 cp ps-edit.sh /etc/profile.d/ps-edit.sh
+cp colors.sh  /etc/profile.d/colors.sh
